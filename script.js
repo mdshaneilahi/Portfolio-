432d7888ec52364535941c2a52e6266379a58c90
MD SHANE ILAHI <86158643+mdshaneilahi@users.noreply.github.com>
2023-09-11 20:22:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function () {
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Web-Developer", "Web-Designer", "Graphic-Desiner"],
+        typeSpeed: 130,
+        backSpeed: 90,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Web-Developer", "Web-Designer", "Graphic-Desiner"],
+        typeSpeed: 130,
+        backSpeed: 90,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
+var tl = gsap.timeline();
+
+tl
+
+    .from(".home-content .text-1", {
+        x: -400,
+        duration: 1,
+    })
+    .from(".home-content #name", {
+        opacity: 0,
+        scale: 1.4,
+        duration: 0.8,
+    })
+    .from(".home-content #text1", {
+        opacity: 100,
+        scale: 0,
+        duration: 1,
+    })
+    .from(".home-content #button ", {
+        opacity: 100,
+        scale: 0,
+        duration: 1,
+    })
+    
+
+
+
+
+
+
+
+
